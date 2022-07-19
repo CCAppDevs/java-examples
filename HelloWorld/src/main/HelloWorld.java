d4ff5105888ce62742e63dac9c4102c0080b1661
jesse harlan <gotsanity@gmail.com>
2022-07-19 11:47:30 -0700

    first commit

---
+++ HelloWorld/src/main/HelloWorld.java
@@ -0,0 +1,42 @@
+package main;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+		// primitive types
+		/*
+		 * Start with a lower case letter
+		 * All immutable:
+		 * 
+		 * byte
+		 * short
+		 * int
+		 * long
+		 * float
+		 * double
+		 * boolean
+		 * char
+		 * */
+		int number = 0;
+		
+		int broken;
+		
+		System.out.println(number);
+		
+		
+		
+		// reference types
+		/*
+		 * Start with an upper case letter (example String)
+		 * string (this is a special type)
+		 * classes
+		 * structs (doesnt exist in java)
+		 * enumerations
+		 * arrays
+		 * 
+		 * basically anything that is not a primitive and can be nulled
+		 * */
+	}
+}
